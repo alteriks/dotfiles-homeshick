8a4037180c3e129c9a51bda438432ffad1f110a0
Krzysztof Dajka <krzysztof.dajka.ext@gigaset.com>
2018-01-16 22:16:13 +0100

    screenlayout

---
+++ home/.screenlayout/3_external_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP3 --off --output eDP1 --off --output DP1 --mode 1920x1200 --pos 3840x0 --rotate left --output HDMI3 --mode 1920x1200 --pos 0x288 --rotate normal --output HDMI2 --mode 1920x1200 --pos 1920x288 --rotate normal --output HDMI1 --off --output VGA1 --off --output DP2 --off
