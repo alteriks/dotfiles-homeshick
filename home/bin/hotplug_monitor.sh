41d3bdf28eb0d9b7be82772cc6653a0ee58197fe
Krzysztof Dajka <krzysztof.dajka.ext@gigaset.com>
2018-03-02 19:07:59 +0100

    hotplug

--- home/bin/hotplug_monitor.sh
+++ home/bin/hotplug_monitor.sh
@@ -7,28 +7,16 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u ${X_USER})/bus
 
 function connect()
 {   
-#
-/usr/bin/autorandr off
+  #/usr/bin/autorandr off
   sleep 1
-/usr/bin/autorandr -c --force
-#xrandr --output VIRTUAL1 --off --output DP1 --mode 3440x1440 --pos 1366x0 --rotate normal --output eDP1 --primary --mode 1366x768 --pos 0x672 --rotate normal --output DP3 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
-
-    #dual monitor - HDMI1 at right - xfce panel on left (LVDS1)
-    #xrandr --output HDMI1 --right-of LVDS1 --preferred  --output HDMI1 --primary >> /tmp/screen
-
-    #Set xfce panel on primary monitor
-    #xfconf-query -v -c xfce4-panel -p /panels/panel-0/output-name -s HDMI1 
+  /usr/bin/autorandr -c 
 }
 
 function disconnect(){
-#/usr/bin/autorandr -l laptop
-/home/alteriks/.screenlayout/laptop.sh
-     #xrandr --output HDMI1 --off
-     #Set xfce panel on LVDS1
-     #xfconf-query -c xfce4-panel -p /panels/panel-0/output-name -s LVDS1
+  #/usr/bin/autorandr -l laptop
+  /home/${X_USER}/.screenlayout/laptop.sh
 }
 
-#if [ $(cat /sys/class/drm/card0-DP-1/status) == "connected" ] ; then
 if [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -ge 1 ] ; then
   DISPLAY_SETUP=`autorandr 2>&1 | grep detected | cut -f1 | awk '{print $1}'`
   if [[ ${DISPLAY_SETUP} =~ 'docked' ]]; then
@@ -38,13 +26,9 @@ if [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -g
   fi
   sleep 1
   connect
-#  TODO: indent
-#  TODO: qualia or i3-msg to move workspaces
-#i3msg-reload
 
 elif [ $(cat /sys/class/drm/card0-{DP,HDMI,VGA}*/status | grep -c '^connected') -eq 0 ] ; then
-#elif [ $(cat /sys/class/drm/card0-DP-1/status) == "disconnected" ] ; then
-	sleep 1
+  sleep 1
   disconnect
 else 
   exit
